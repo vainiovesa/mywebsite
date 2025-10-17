962543278ad5427d692ac4527587d37607fd34bc
vainiovesa <vesa.vainio@helsinki.fi>
2025-10-17 10:05:23 +0300

    Add 404 page

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def index():
 def test():
     return render_template("test.html")
 
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("404.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
