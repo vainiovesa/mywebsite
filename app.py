92ed6bf4b9ee0ec1e6d978193e48455061fd1a73
vainiovesa <vesa.vainio@helsinki.fi>
2025-10-20 10:28:51 +0300

    Add language choice

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template, redirect
 from werkzeug.middleware.proxy_fix import ProxyFix
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 @app.route("/")
 def index():
+    language_default()
     return render_template("index.html")
 
 @app.route("/sus")
 def test():
     return render_template("test.html")
 
+@app.route("/languages/<choice>")
+def languages(choice):
+    session["language"] = "fi"
+
+    if choice == "en":
+        session["language"] = "en"
+
+    return redirect("/")
+
 @app.errorhandler(404)
 def not_found(e):
   return render_template("404.html")
+
+def language_default():
+    if "language" not in session:
+        session["language"] = "fi"
 
 if __name__ == "__main__":
     app.run(debug=True)
